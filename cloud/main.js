f1ce4b6a61000cd220874b389a271e1570cc4bbf
Aditya Mangal <adityamangal410@gmail.com>
2015-02-24 00:43:28 -0800

    Added API to pull venues based on location in chunks

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
-var moment = require('moment');
 var metadata = require('cloud/metadata.js');
+var publicAPI = require('cloud/publicAPI.js');
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
@@ -8,35 +8,9 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-function LocData(locData) {
-  this.name = locData.name;
-  this.vaddress = locData.location.formattedAddress;
-}
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
-Parse.Cloud.define("foursquare", function(request, response) {
-
-//https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=ZGXMWBZGHSGHWSCYQXLNYOEWP31N1RBKBNDZRV24E2AWDTXR&client_secret=QXCGOG0F4BILVX5JY0BNKMWIJLZJXYRVD5POZ1BZRHOZ0RLV&v=20150219
-
-return Parse.Cloud.httpRequest({
-  url: 'https://api.foursquare.com/v2/venues/search',
-  params: {
-    ll : '40.7,-74',
-    client_id : 'ZGXMWBZGHSGHWSCYQXLNYOEWP31N1RBKBNDZRV24E2AWDTXR',
-    client_secret : 'QXCGOG0F4BILVX5JY0BNKMWIJLZJXYRVD5POZ1BZRHOZ0RLV',
-    v : '20150219'
-  }
-}).then(function (httpResponse) {
-//    response.success(httpResponse.text);
-  var locData = JSON.parse(httpResponse.text);
-  var locList = [];
-  _.each(locData.response.venues, function(selectedLoc) {
-    locList.push(new LocData(selectedLoc));
-  });
-  response.success(locList);
-
-});
-
+Parse.Cloud.define("searchFoursquareByLocation", function(request, response) {
+  publicAPI.searchFoursquareByLocation(request, response);
 });
 
 Parse.Cloud.define("getAndSaveCategories", function(request, response) {
